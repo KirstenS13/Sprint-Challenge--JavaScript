387bb9b57b8e7af72b9283f21c2a46102a8fb913
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-03-22 19:25:06 -0400

    passed the dinosaur object section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -39,20 +39,25 @@ const velObject = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrObject.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velObject.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegObject.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrObject.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+function roar() {
+  return 'RAWERSRARARWERSARARARRRR!';
+}
+
+console.log(roar());
 
 
 // ==== Arrays ====
