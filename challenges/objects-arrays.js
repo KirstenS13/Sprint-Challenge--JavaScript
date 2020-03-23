f400dd3166f6a72eb161cb87c0dad29ef8d52cf3
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-03-22 20:19:02 -0400

    passed array request 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -105,9 +105,11 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
 
-graduates.filter((arrItem) => {
-
-})
+for (let i = 0; i<graduates.length; i++) {
+  if (graduates[i].university.indexOf('Uni') >= 0) {
+    unisWithUni.push(graduates[i]);
+  }
+}
 
 console.log(unisWithUni);
 
